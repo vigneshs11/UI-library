6b80e9f2e1cfac9cf897e8c52fc063b0a53bc521
vigneshs11 <vigneshswaminathan11@gmail.com>
2021-12-06 07:41:13 +0530

    first commit

---
+++ acme/acme-components/demo/src/App.js
@@ -0,0 +1,16 @@
+import Button from 'acme-components/Button';
+import Input from 'acme-components/Input';
+
+import 'acme-components/theme.css';
+
+function App() {
+  return (
+    <>
+      <h1>ACME’s Components</h1>
+      <Input />
+      <Button>Button component</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ acme/acme-components/src/Input.js
@@ -0,0 +1,10 @@
+// Input.js
+import React from 'react';
+
+const Input = (props) => {
+  return (
+    <input {...props} className="acme-input" />
+  )
+}
+
+export default Input;
